e29bbc37f3570456f1de9310d03e3713d7f5178a
KUAI <2542862169@qq.com>
2020-08-30 21:57:18 +0800

    add info & wechat

--- mysql.sql
+++ mysql.sql
@@ -6,13 +6,17 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `ekm_auth_user`;
-CREATE TABLE `ekm_auth_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` text NOT NULL,
-  `pass` text NOT NULL,
-  `cookie` text NOT NULL,
-  `reg_time` int(11) NOT NULL,
-  `login_time` int(11) NOT NULL,
-  `ip` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `ekm_auth_user`
+(
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `user`       text    NOT NULL,
+    `pass`       text    NOT NULL,
+    `cookie`     text    NOT NULL,
+    `reg_time`   int(11) NOT NULL,
+    `login_time` int(11) NOT NULL,
+    `ip`         text    NOT NULL,
+    `wechat`     text,
+    `info`       text,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
